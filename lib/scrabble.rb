ec10f7a5011dc09050114d957b8e2c8d4b4fc9d3
Josh Thompson <thompsonjoshd@gmail.com>
2017-02-17 12:29:57 -0700

    finished

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Scrabble
   def score(word)
     word.upcase.chars.map do |letter|
@@ -15,5 +17,40 @@ class Scrabble
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
       "Y"=>4, "Z"=>10
     }
+  end
+
+  def score_with_multipliers(word, letter_multiplier = 1, word_multiplier = 1)
+    score = 0
+    long_word_bonus = 10
+    word.upcase.chars.map.with_index do |char, index|
+      score += (point_values[char] * letter_multiplier[index])
+    end
+
+    score += long_word_bonus if word.length >= 7
+    score * word_multiplier
+  end
+
+  def highest_scoring_word(word_array)
+    words_and_scores = {}
+    word_array.map do |word|
+      words_and_scores[word] = score_with_multipliers(word)
+    end
+    words_and_scores.sort_by { |word, score| score && word.length }
+
+    top_scores = words_and_scores.select{|word,score| score == words_and_scores.values.max }
+    if top_scores.select {|word, score| word.length == 7 }.any?
+      top_score = top_scores.select {|word, score| word.length == 7 }
+    else
+      top_score = top_scores.min_by {|key, value| key.length }
+    end
+
+
+    if top_score.kind_of?(Array)
+      [top_score].to_h.keys.join
+    else
+      top_score.keys.join
+    end
+
+
   end
 end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -9,4 +9,42 @@ class ScrabbleTest < Minitest::Test
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
+
+  def test_score_with_letter_multipliers
+
+    game = Scrabble.new
+    score = game.score_with_multipliers('hello', [1,2,1,1,1])
+    assert_equal 9, score
+  end
+
+  def test_score_with_word_multipliers
+
+    game = Scrabble.new
+    score = game.score_with_multipliers('hello', [1,2,1,1,1], 2)
+    assert_equal 18, score
+  end
+
+  def test_long_word_bonus
+    game = Scrabble.new
+    score = game.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
+    assert_equal 58, score
+  end
+
+  def test_highest_scoring_word
+    game = Scrabble.new
+    highest_scoring_word = game.highest_scoring_word(['home', 'word', 'hello', 'sound'])
+    assert_equal "home", highest_scoring_word
+  end
+
+  def test_highest_scoring_word_of_same_score
+    game = Scrabble.new
+    highest_scoring_word = game.highest_scoring_word(['hello', 'word', 'sound'])
+    assert_equal "word", highest_scoring_word
+  end
+
+  def test_highest_scoring_word_of_tie_can_be_seven_letters
+    game = Scrabble.new
+    highest_scoring_word = game.highest_scoring_word(['home', 'word', 'silence'])
+    assert_equal "silence", highest_scoring_word
+  end
 end
